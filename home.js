da632ed88af6a0a7b59b1c7cbf051262ca1a7015
Jack Skiles <jackskiles420@gmail.com>
2022-03-16 19:53:12 -0400

    updated some javascript

--- home.js
+++ home.js
@@ -1,23 +1,47 @@
 var moon = document.getElementById('moon')
-function scrolListener(e){
-  var screenTop = document.scrollingElement.scrollTop;
-  var screenBottom = screenTop + innerHeight;
-  var moonTop = moon.getBoundingClientRect().top
-  
-  if ( moonTop < screenBottom && moonTop < screenTop)
-  {
-      moon.classList.add("moon");
-      moon.classList.remove("firstMoon");
-    console.log("Hello");
-  }
-  else if ( moonTop > screenBottom && moonTop > screenTop)
-  {
-      moon.classList.add("firstMoon");
-      moon.classList.remove("moon");
-    console.log("Hello");
-  }
-  
-}
+var house = document.getElementById('house')
+var lamp = document.getElementById('lamp')
+
+let animations = [moon, house, lamp];
+
+console.log(animations[0])
 
 
-document.onscroll = scrolListener
+// function scrolListener(e) {
+
+//     var screenTop = document.scrollingElement.scrollTop;
+//     var screenBottom = screenTop + innerHeight;
+//     let moonTop = moon.getBoundingClientRect().top
+//     let houseTop = house.getBoundingClientRect().top
+//     let lampTop = lamp.getBoundingClientRect().top
+
+//     for (let i = 0; i <= animations.length; i++) {
+//         let item = animations[i] + "Top";
+//         console.log(item);
+//         if (item < screenBottom && item < screenTop) {
+//             animations[i].classList.add(animations[i]);
+//             animations[i].classList.remove("first" + animations[i]);
+//             console.log("Hello");
+//         }
+//         else if (moonTop > screenBottom && moonTop > screenTop) {
+//             animations[i].classList.add(animations[i]);
+//             animations[i].classList.remove("first" + animations[i]);
+//             console.log("Hello");
+//         }
+//     }
+
+//     // if (moonTop < screenBottom && moonTop < screenTop) {
+//     //     moon.classList.add("moon");
+//     //     moon.classList.remove("firstMoon");
+//     //     console.log("Hello");
+//     // }
+//     // else if (moonTop > screenBottom && moonTop > screenTop) {
+//     //     moon.classList.add("firstMoon");
+//     //     moon.classList.remove("moon");
+//     //     console.log("Hello");
+//     // }
+
+// }
+
+
+// document.onscroll = scrolListener
